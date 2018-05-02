ae2c9811d66fb8982696f0e180a237c5c9d02648
Mikkel Valentin Sørensen <mvs@codehouse.com>
2018-05-02 12:43:24 +0200

    Added multi invoke test

---
+++ source/MikValSor.ImmutableValidator.Test/ImmutableValidatorMultiValidateTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace MikValSor.Immutable.Test
+{
+	[TestFixture]
+	public class ImmutableValidatorMultiValidateTest
+	{
+		[Test]
+		public void ImmutableValidator_object_StringAndStringArray()
+		{
+			var validator =	new ImmutableValidator();
+			var sArray = new string[0];
+			var s = string.Empty;
+
+			//Act
+			var s1 = validator.IsImmutable(s);
+			var sArray1 = validator.IsImmutable(sArray);
+			var s2 = validator.IsImmutable(s);
+			var sArray2 = validator.IsImmutable(sArray);
+
+			//Assert
+			Assert.IsTrue(s1);
+			Assert.IsFalse(sArray1);
+			Assert.IsTrue(s2);
+			Assert.IsFalse(sArray2);
+		}
+	}
+}
